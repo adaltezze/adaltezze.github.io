970eee6582e831040e68443d918bf7dadecdf21a
adaltezze <adaltezze@gmail.com>
2018-01-18 19:59:00 +0500

    btn

---
+++ kappe/js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function() {
+  var btn = document.querySelector(".btn-menu");
+  var menu = document.querySelector(".container-menu");
+
+  btn.addEventListener("click", function() {
+    menu.classList.add = "active";
+  });
+});
