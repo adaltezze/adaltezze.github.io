7b3ed3695f3c89c2a10d87555025b0b276562e64
adaltezze <adaltezze@gmail.com>
2019-04-01 17:40:57 +0500

    init start page | add react, javascript pages

--- kappe/js/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", function() {
-  var btn = document.querySelector(".btn-menu");
-  var menu = document.querySelector(".container-menu");
-
-  btn.addEventListener("click", function() {
-    menu.classList.toggle("active");
-  });
-});
