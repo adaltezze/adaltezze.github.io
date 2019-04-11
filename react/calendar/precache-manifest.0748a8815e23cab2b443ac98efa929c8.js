803afde78c8eac23e6d645c636047390710149d6
adaltezze <adaltezze@gmail.com>
2019-04-12 03:18:04 +0500

    add calendar on react

---
+++ react/calendar/precache-manifest.0748a8815e23cab2b443ac98efa929c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cb5496b73b3ef544231d",
+    "url": "/static/js/main.96bb2c26.chunk.js"
+  },
+  {
+    "revision": "276c0ba0063aa3e72172",
+    "url": "/static/js/2.b3ae52f1.chunk.js"
+  },
+  {
+    "revision": "276c0ba0063aa3e72172",
+    "url": "/static/css/2.b57cd7d7.chunk.css"
+  },
+  {
+    "revision": "0d8717b991ae739ccd9b98b33a967a25",
+    "url": "/index.html"
+  }
+];
