6699cc7d23419f2e6d3008cc97ba6f73f93e1240
adaltezze <adaltezze@gmail.com>
2019-05-10 14:36:56 +0500

    update

---
+++ precache-manifest.0064007b5cd1880541d44ba4df897737.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dd37593abbd8c3d9598",
+    "url": "/adaltezze/react-video-portal/static/js/runtime~main.f48daece.js"
+  },
+  {
+    "revision": "0fe4633ace4dd5c44a3b",
+    "url": "/adaltezze/react-video-portal/static/js/main.cbc7d820.chunk.js"
+  },
+  {
+    "revision": "615378149c9d8cf34dbf",
+    "url": "/adaltezze/react-video-portal/static/js/2.3a19c195.chunk.js"
+  },
+  {
+    "revision": "0fe4633ace4dd5c44a3b",
+    "url": "/adaltezze/react-video-portal/static/css/main.ff3128c6.chunk.css"
+  },
+  {
+    "revision": "615378149c9d8cf34dbf",
+    "url": "/adaltezze/react-video-portal/static/css/2.b185cdad.chunk.css"
+  },
+  {
+    "revision": "5857201f6d2ccbd226e631ab578c1a60",
+    "url": "/adaltezze/react-video-portal/index.html"
+  }
+];
